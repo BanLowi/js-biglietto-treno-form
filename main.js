8be276b262715a53dae6342fd0172fed2f4a61fb
BanLowi <lowi40k@gmail.com>
2025-10-15 17:30:35 +0200

    checkPrice function + event listner for submitting user input

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 const ticket_default = 0.21;
 
-let user_age = document.getElementById("age");
-let user_distance = document.getElementById("distance");
-console.log(user_age);
-console.log(user_distance);
+const user_age = document.getElementById("age");
+const user_distance = document.getElementById("distance");
+const buttonEl = document.querySelector("button");
 
+
+
+buttonEl.addEventListener("click", () => {
+    const ageValue = user_age.value;
+    const distanceValue = user_distance.value;
+    console.log(checkPrice(ageValue, distanceValue));
+})
 
 
 
 function checkPrice(age, distance) {
     let ticket_price;
     
-    if (age.value >= 65) {
-        ticket_price = (ticket_default * distance.value) - (ticket_default * distance.value * 0.4);
+    if (age >= 65) {
+        ticket_price = (ticket_default * distance - (ticket_default * distance * 0.4));
     
     }
-    else if (age.value < 18) {
-        ticket_price = (ticket_default * distance.value) - (ticket_default * distance.value * 0.2);
+    else if (age < 18) {
+        ticket_price = (ticket_default * distance) - (ticket_default * distance * 0.2);
     }
     else {
-        ticket_price = ticket_default * distance.value;
+        ticket_price = ticket_default * distance;
     }
 
-    return `Il tuo biglietto costa: ${ticket_price.toFixed(2)}`
+    return ticket_price.toFixed(2)
 }
-
-console.log(checkPrice(user_age, user_distance));
