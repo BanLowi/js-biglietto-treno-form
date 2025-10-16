9bc3c916ff8909dd77fb83d2e254a3bb45b60e93
BanLowi <lowi40k@gmail.com>
2025-10-16 09:35:48 +0200

    form card style

--- main.js
+++ main.js
@@ -4,9 +4,26 @@ const user_age = document.getElementById("age");
 const user_distance = document.getElementById("distance");
 const buttonEl = document.querySelector("button");
 const formEl = document.querySelector("form");
+const rowEl = document.querySelector("row");
 
-const resultDisplay = document.createElement("p");
-formEl.append(resultDisplay);
+const resultDisplay = document.createElement(`
+                <div class="card col-3 text-center">
+                
+                <div class="card-header bg-transparent mb-2">
+                    <h4>Compra un biglietto!</h4>
+                </div>
+                <form>
+                <select id="age" class="form-select mb-2">
+                    <option value="Maggiorenne">Maggiorenne</option>
+                    <option value="Minorenne">Minorenne</option>
+                    <option value="Anziano">Anziano</option>
+                </select>
+                <input type="text" id="distance" placeholder="Inserisci distanza" class="form-control mb-2">
+                <button class="btn btn-sm btn-primary" type="submit">INVIA</button>
+                </form>
+                </div>
+            `);
+rowEl.append(resultDisplay);
 
 
 // Al click di "INVIA" salvo i dati di input e stampo il risultato di checkPrice con i dati
